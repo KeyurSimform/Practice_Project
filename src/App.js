da0ee3d616cc516c6b705185f41e1d2f079d374e
keyur <keyur.k@siformsolutions.com>
2022-07-01 14:56:43 +0530

    Adding the Header section to all the page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from "../src/Pages/HomePage/HomePage";
 import HatsPage from "./Pages/HatsPage/HatsPage";
 import { Route, Routes } from "react-router-dom";
 import Shop from "./Pages/ShopPage/Shop";
+import Header from "./Componenet/Header/Header";
 
 function App() {	
 	return (
 		<div>
+		<Header/>
 			<Routes>
 				<Route path="/" element={<HomePage/>}/>
 				<Route path="/shop" element={<Shop/>}/>
